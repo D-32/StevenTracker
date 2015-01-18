ddd3493f217ab54fa7f04edc29efa992412aa0f9
Dylan Marriott <info@d-32.com>
2015-01-18 15:55:12 +0100

    Don't show app on launch

--- app/src/main/java/com/dylanmarriott/steventracker/LocationWebService.java
+++ app/src/main/java/com/dylanmarriott/steventracker/LocationWebService.java
@@ -1,7 +1,6 @@
 package com.dylanmarriott.steventracker;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import android.os.AsyncTask;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -9,30 +8,31 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.os.AsyncTask;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class LocationWebService extends AsyncTask<String, String, Boolean> {
 
-public LocationWebService() {
-}
+    public LocationWebService() {
+    }
 
-@Override
-protected Boolean doInBackground(String... arg0) {
+    @Override
+    protected Boolean doInBackground(String... arg0) {
 
-	try {
-		HttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost("http://192.168.1.101:8000/");
-		post.setEntity(new StringEntity(arg0[1], "UTF8"));
-		post.setHeader("Content-type", "application/json");
-		client.execute(post);
-	} catch (UnsupportedEncodingException e) {
-		e.printStackTrace();
-	} catch (ClientProtocolException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	
-    return null;
-}
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost("http://192.168.1.101:8000/");
+            post.setEntity(new StringEntity(arg0[1], "UTF8"));
+            post.setHeader("Content-type", "application/json");
+            client.execute(post);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
--- app/src/main/java/com/dylanmarriott/steventracker/MainActivity.java
+++ app/src/main/java/com/dylanmarriott/steventracker/MainActivity.java
@@ -1,41 +1,16 @@
 package com.dylanmarriott.steventracker;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-        
         Intent intent = new Intent(this, LocationService.class);
         startService(intent);
-    }
-    
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 
 }
